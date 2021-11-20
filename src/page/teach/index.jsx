5d0698db638eed7b211e31ca389ec1f1ac8b8f94
lruler <1204312199@qq.com>
2021-11-20 10:42:56 +0800

    1

--- src/page/teach/index.jsx
+++ src/page/teach/index.jsx
@@ -4,10 +4,11 @@ import './index.css'
 export default function Teach() {
   return (
     <>
-      <iframe
+      <video src="https://kubesphere-community.pek3b.qingstor.com/videos/kubesphere-demo-cn.mp4" controls autoPlay></video>
+      {/* <iframe
         title=" "
         src="//player.bilibili.com/player.html?aid=460769491&bvid=BV1A5411g7az&cid=344269894&page=1"
-        scrolling="no" border="0" frameBorder="no" framespacing="0" allowFullScreen={true}> </iframe>
+        scrolling="no" border="0" frameBorder="no" framespacing="0" allowFullScreen={true}> </iframe> */}
     </>
   )
 }
